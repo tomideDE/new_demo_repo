72e4766126413266d3855f42dda1269aee197e8c
Ayotomide Akin-Ojo <160484190+tomideDE@users.noreply.github.com>
2025-06-02 15:29:54 +0100

    First commit

---
+++ kitchen_items.py
@@ -0,0 +1,21 @@
+# kitchen_items.py
+
+# Create a list of kitchen items
+kitchen_items = [
+    "Spoon",
+    "Fork",
+    "Knife",
+    "Plate",
+    "Cup",
+    "Pan",
+    "Pot",
+    "Cutting board",
+    "Blender",
+    "Microwave",
+    "Sink"
+]
+
+# Print the list
+print("List of Kitchen Items:")
+for item in kitchen_items:
+    print("- " + item)
